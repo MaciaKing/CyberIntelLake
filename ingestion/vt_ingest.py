a51b1be7690cf5ee840323735ee9194e2d015a39
MaciaKing <maciasalvasalva@gmail.com>
2025-08-17 17:16:18 +0200

    [feat]: add try/finally for guaranteed batch progress save and replace prints with logger info

--- ingestion/vt_ingest.py
+++ ingestion/vt_ingest.py
@@ -5,7 +5,7 @@ from models.database import SessionLocal
 from models.virus_total import VirusTotal
 from models.file_reader import FileReader
 from pathlib import Path
-import pdb
+from datetime import datetime
 
 # File to extract data
 file_path_to_extract = Path(__file__).parent / '../data_to_extract/black_list_domain.txt'
@@ -21,32 +21,38 @@ OUTPUT_FILE = Path(__file__).parent / F"../data/bronze/virus_total/virus_total_b
 
 request_day_counter = 0
 
-while request_day_counter <= vt.MAX_REQUEST_PER_DAY:
-    domain_to_query = fr.read_line(progress.last_line_read)
-    # Se ha finalizado la lectura del fichero
-    if not domain_to_query:
-        break
-    record = vt.make_domain_query(domain_to_query)
+try:
+    while request_day_counter <= vt.MAX_REQUEST_PER_DAY:
+        domain_to_query = fr.read_line(progress.last_line_read)
+        # The file has been read completely.
+        if not domain_to_query:
+            break
+        record = vt.make_domain_query(domain_to_query)
 
-    # Encapsular el record con el dominio
-    wrapped_record = {
-        "id": domain_to_query,
-        "file_extracted": file_path_to_extract,
-        "response": record
-    }
+        # Encapsulate the record with the domain and the
+        # name of the file from which it was extracted.
+        wrapped_record = {
+            "id": domain_to_query,
+            "file_extracted": str(file_path_to_extract),
+            "response": record
+        }
 
-    # Guardar cada record en NDJSON (append)
-    # batch
-    with open(OUTPUT_FILE, "a", encoding="utf-8") as f:
-        f.write(json.dumps(wrapped_record, ensure_ascii=False) + "\n")
+        # Save record as NDJSON
+        # batch
+        with open(OUTPUT_FILE, "a", encoding="utf-8") as f:
+            f.write(json.dumps(wrapped_record, ensure_ascii=False) + "\n")
 
-    # Save last line read of VT extraction
-    progress.last_line_read += 1
+        # Save last line read of VT extraction
+        progress.last_line_read += 1
+        progress.save(database_session)
+        request_day_counter += 1
+
+        print(f"{domain_to_query} processed")
+
+        # Wait time between requests
+        time.sleep(vt.get_waiting_time_between_requests())
+
+finally:
+    progress.last_batch_number_extracted +=1
     progress.save(database_session)
-    request_day_counter += 1
-
-    # Wait time between requests
-    time.sleep(vt.get_waiting_time_between_requests())
-
-progress.last_batch_number_extracted +=1
-progress.save(database_session)
+    print(f"ELT for Virus Total finished at {datetime.now()}")
